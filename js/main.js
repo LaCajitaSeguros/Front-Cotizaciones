b05180ebdf3a8e22f1970c90f4b4ea7f6b0b0443
LeonelEstrella <ealanleonel@gmail.com>
2024-06-11 14:59:09 -0300

    Put all in one page

--- js/main.js
+++ js/main.js
@@ -2,38 +2,53 @@ const dropdownAnio = document.getElementById('dropdown-anio');
 const dropdownMarca = document.getElementById('dropdown-marca');
 const dropdownModelo = document.getElementById('dropdown-modelo');
 const dropdownVersion = document.getElementById('dropdown-version');
-const nextButton = document.getElementById('next-button');
+const cotizarButton = document.getElementById('cotizar-button');
+const dropdownLocalidad = document.getElementById('dropdown-localidad');
+const fechaNacimientoInput = document.getElementById("fecha-nacimiento");
+let edadUsuario = null; // Variable para almacenar la edad del usuario
+const tieneGNCInput = document.getElementById("tiene-gnc"); // Obtener el elemento del checkbox
+let tieneGNC = false; // Variable global para almacenar el estado del checkbox
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     const apiMarcaUrl = 'https://localhost:7061/api/Marca';
+    const apiLocalidadUrl = 'https://localhost:7061/api/Localidad';
 
-    // Function to check if all dropdowns have a selected value
+    // Chequear si hay que deshabilitar o no el boton de siguiente. Si tiene todos los valores se habilita
     function checkDropdowns() {
-        if (dropdownAnio.value && dropdownMarca.value && dropdownModelo.value && dropdownVersion.value) {
-            nextButton.disabled = false;
+        if (
+            dropdownAnio.value &&
+            dropdownMarca.value &&
+            dropdownModelo.value &&
+            dropdownVersion.value &&
+            dropdownLocalidad.value &&
+            fechaNacimientoInput.value &&
+            edadUsuario >= 18 // Asegúrate de que el usuario sea mayor de 18 años
+        ) {
+            cotizarButton.disabled = false;
         } else {
-            nextButton.disabled = true;
+            cotizarButton.disabled = true;
         }
     }
 
-    // Populate dropdown-anio with years dynamically
-    const startYear = 1989;
+    // Completar el dropdown de año desde 2009 hasta 2024
+    const startYear = 2009;
     const endYear = 2024;
 
-    for (let year = startYear; year <= endYear; year++) {
+    for (let year = endYear; year >= startYear; year--) {
         const option = document.createElement('option');
         option.value = year;
         option.textContent = year;
         dropdownAnio.appendChild(option);
     }
 
-    dropdownAnio.addEventListener('change', function() {
+    dropdownAnio.addEventListener('change', function () {
         // Llama a la función para actualizar el almacenamiento local
         actualizarLocalStorage();
+        checkDropdowns();
     });
 
-    // Fetch data from the API endpoint for Marca
+    // Request de marca
     fetch(apiMarcaUrl)
         .then(response => response.json())
         .then(data => {
@@ -48,21 +63,81 @@ document.addEventListener('DOMContentLoaded', function() {
             console.error('Error fetching data:', error);
         });
 
-    // Add event listener to marca dropdown to fetch modelos based on selected marca
-    dropdownMarca.addEventListener('change', function() {
+    // Request de localidad
+    fetch(apiLocalidadUrl)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(item => {
+                const option = document.createElement('option');
+                option.value = item.id;
+                option.textContent = item.nombre;
+                dropdownLocalidad.appendChild(option);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+
+    // Función para verificar si el usuario tiene al menos 18 años
+    function verificarEdad(fecha) {
+        const hoy = new Date();
+        const fechaNacimiento = new Date(fecha);
+        let edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
+        const mes = hoy.getMonth() - fechaNacimiento.getMonth();
+
+        if (mes < 0 || (mes === 0 && hoy.getDate() < fechaNacimiento.getDate())) {
+            edad--;
+        }
+
+        return edad;
+    }
+
+    // Agregamos un evento de cambio al input de fecha de nacimiento
+    fechaNacimientoInput.addEventListener("change", function () {
+        edadUsuario = verificarEdad(this.value); // Almacenamos la edad calculada en la variable
+
+        // Si la edad es menor a 18, mostramos una alerta y vaciamos el campo de fecha
+        if (edadUsuario < 18) {
+            alert("Debes ser mayor de 18 años para continuar.");
+            this.value = ""; // Vaciamos el campo de fecha
+        }
+        checkDropdowns();
+    });
+
+    // Función para establecer el valor predeterminado si el checkbox está vacío
+    function establecerValorPredeterminado() {
+        tieneGNC = tieneGNCInput.checked;
+    }
+
+    // Agregamos un evento de carga al cargar la página
+    window.addEventListener("load", function () {
+        establecerValorPredeterminado();
+    });
+
+    // Agregamos un evento de cambio al checkbox
+    tieneGNCInput.addEventListener("change", function () {
+        tieneGNC = this.checked; // Actualizamos el valor de la variable global
+
+        // Aquí puedes hacer lo que necesites con el valor del checkbox
+        console.log("El valor de tieneGNC es:", tieneGNC);
+    });
+
+
+    // Mostrar modelos según la marca que se seleccionó
+    dropdownMarca.addEventListener('change', function () {
         const selectedMarcaId = dropdownMarca.value;
         const apiModeloUrl = `https://localhost:7061/api/Modelo/${selectedMarcaId}`;
 
         fetch(apiModeloUrl)
             .then(response => response.json())
             .then(data => {
-                // Clear previous options
+                // Si se cambio de Marca, vuelve a mostrar el texto por default
                 dropdownModelo.innerHTML = '<option value="" disabled selected>Buscar modelo</option>';
 
                 // Clear dropdown-version and add default option
                 dropdownVersion.innerHTML = '<option value="" disabled selected>Buscar versión</option>';
 
-                // Populate dropdown-modelo with fetched data
+                // Cargar el dropdown de Marca con toda la información de la request
                 data.forEach(item => {
                     const option = document.createElement('option');
                     option.value = item.id;
@@ -70,17 +145,17 @@ document.addEventListener('DOMContentLoaded', function() {
                     dropdownModelo.appendChild(option);
                 });
 
-                // Enable the next button if all dropdowns have a selected value
+                // Habilita el botón siguiente si todos los campos tienen un valor
                 checkDropdowns();
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
             });
-            actualizarLocalStorage();
+        actualizarLocalStorage();
     });
 
-    // Add event listener to marca dropdown to fetch versiones based on selected modelo
-    dropdownModelo.addEventListener('change', function() {
+    // Buscar versiones según el modelo
+    dropdownModelo.addEventListener('change', function () {
         const selectedModeloId = dropdownModelo.value;
         console.log(selectedModeloId);
         const apiVersionUrl = `https://localhost:7061/api/Version/${selectedModeloId}`;
@@ -88,10 +163,10 @@ document.addEventListener('DOMContentLoaded', function() {
         fetch(apiVersionUrl)
             .then(response => response.json())
             .then(data => {
-                // Clear previous options
+                // En caso de que cambie la versión, volver a poner el mensaje por default del dropdown de versión
                 dropdownVersion.innerHTML = '<option value="" disabled selected>Buscar versión</option>';
 
-                // Populate dropdown-version with fetched data
+                // Completar el dropdown de versión
                 data.forEach(item => {
                     const option = document.createElement('option');
                     option.value = item.id;
@@ -99,21 +174,21 @@ document.addEventListener('DOMContentLoaded', function() {
                     dropdownVersion.appendChild(option);
                 });
 
-                // Enable the next button if all dropdowns have a selected value
+                // Chequear si se puede habilitar el botón de siguiente
                 checkDropdowns();
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
             });
-            actualizarLocalStorage();
+        actualizarLocalStorage();
     });
 
-    dropdownVersion.addEventListener('change', function() {
+    dropdownVersion.addEventListener('change', function () {
         // Llama a la función para actualizar el almacenamiento local
         actualizarLocalStorage();
     });
 
-    // Add event listeners to dropdowns to check their values on change
+    // Chequear si todos los campos tienen valores
     dropdownAnio.addEventListener('change', checkDropdowns);
     dropdownMarca.addEventListener('change', checkDropdowns);
     dropdownModelo.addEventListener('change', checkDropdowns);
@@ -121,24 +196,79 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 //Enviar al usurio a la siguiente página
-document.getElementById('next-button').addEventListener('click', function() {
-    window.location.href = './cotizar-page2.html';
+document.getElementById('cotizar-button').addEventListener('click', function () {
+    enviarSolicitudPOST();
 });
 
-    // Función para actualizar el almacenamiento local con los valores seleccionados
-    function actualizarLocalStorage() {
-        localStorage.setItem('selectedMarcaId', dropdownMarca.value);
-        localStorage.setItem('selectedModeloId', dropdownModelo.value);
-        localStorage.setItem('selectedVersionId', dropdownVersion.value);
-        localStorage.setItem('selectedAnio', dropdownAnio.value);
-    }
+// Función para actualizar el almacenamiento local con los valores seleccionados
+function actualizarLocalStorage() {
+    localStorage.setItem('selectedMarcaId', dropdownMarca.value);
+    localStorage.setItem('selectedModeloId', dropdownModelo.value);
+    localStorage.setItem('selectedVersionId', dropdownVersion.value);
+    localStorage.setItem('selectedAnio', dropdownAnio.value);
+    localStorage.setItem('selectedLocalidadId', dropdownLocalidad.value);
+    localStorage.setItem('fechaNacimiento', fechaNacimientoInput.value);
+    localStorage.setItem('tieneGNC', tieneGNC)
+}
 
-console.log(test);
+// Función para enviar la solicitud POST
+function enviarSolicitudPOST() {
+    const localidad = localStorage.getItem('selectedLocalidadId');
+    const edad = edadUsuario;
+    const selectedAnio = localStorage.getItem('selectedAnio');
+    const selectedMarcaId = localStorage.getItem('selectedMarcaId');
+    const selectedModeloId = localStorage.getItem('selectedModeloId');
+    const selectedVersionId = localStorage.getItem('selectedVersionId');
+    const tieneGNC = localStorage.getItem('tieneGNC') === 'true';
+
+    console.log(localidad);
+    console.log(edad);
+    console.log(selectedAnio);
+    console.log(selectedMarcaId);
+    console.log(selectedModeloId);
+    console.log(selectedVersionId);
+    console.log(tieneGNC);
+
+    const data = {
+        localidad: localidad,
+        edad: edad,
+        automovil: {
+            anioVehiculo: selectedAnio,
+            marcaId: selectedMarcaId,
+            modeloId: selectedModeloId,
+            versionId: selectedVersionId,
+            gnc: tieneGNC
+        }
+    };
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+    fetch('https://localhost:7061/api/Vehiculo', options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al enviar la solicitud');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Solicitud POST enviada con éxito:', data);
+        })
+        .catch(error => {
+            console.error('Error al enviar la solicitud POST:', error);
+        });
+}
+
 
 // Evento beforeunload para restablecer la página al recargar
-window.addEventListener('beforeunload', function(event) {
+window.addEventListener('beforeunload', function (event) {
     dropdownAnio.selectedIndex = '<option value="" disabled selected>Buscar año</option>'; // Restablecer dropdown de año seleccionando la primera opción
-    dropdownMarca.selectedIndex = '<option value="" disabled selected>Buscar marca</option>'; // Restablecer dropdown de marca seleccionando la primera opción
+    dropdownMarca.selectedIndex = '<option value="" disabled selected>Buscar versión</option>'; // Restablecer dropdown de marca seleccionando la primera opción
     dropdownModelo.innerHTML = '<option value="" disabled selected>Buscar modelo</option>'; // Limpiar dropdown de modelo
     dropdownVersion.innerHTML = '<option value="" disabled selected>Buscar versión</option>'; // Limpiar dropdown de versión
     localStorage.clear();
